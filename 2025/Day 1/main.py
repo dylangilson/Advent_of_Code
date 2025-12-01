3bda9d161a4b4709d3a5a4cfc0ba69d3d29c321c
dylangilson <dylan.gilson@outlook.com>
2025-12-01 12:43:58 -0800

    updated Day 1 2025

--- 2025/Day 1/main.py
+++ 2025/Day 1/main.py
@@ -1,15 +1,15 @@
-def part_one(lines):
+def part_one(rotations):
     position = [50]
 
-    print("Part One Solution: " + str(sum([(position.__setitem__(0, (position[0] + (int(line[1:]) if line[0] == 'R' else -int(line[1:]))) % 100) or True) and position[0] == 0 for line in lines])))
+    print("Part One Solution: " + str(sum([(position.__setitem__(0, (position[0] + (int(rotation[1:]) if rotation[0] == 'R' else -int(rotation[1:]))) % 100) or True) and position[0] == 0 for rotation in rotations])))
 
-def part_two(lines):
+def part_two(rotations):
     position = [50]
 
-    print(sum([(lambda step:(position.__setitem__(0, (position[0] + step) % 100), position[0] == 0)[1])((1 if line[0] == 'R' else -1)) for line in lines for _ in range(int(line[1:]))]))
+    print("Part Two Solution: " + str(sum([(lambda step:(position.__setitem__(0, (position[0] + step) % 100), position[0] == 0)[1])((1 if rotation[0] == 'R' else -1)) for rotation in rotations for _ in range(int(rotation[1:]))])))
 
 if __name__ == '__main__':
-    lines = open('input.txt').read().strip().split('\n')
+    rotations = open('input.txt').read().strip().split('\n')
 
-    part_one(lines)
-    part_two(lines)
+    part_one(rotations)
+    part_two(rotations)
